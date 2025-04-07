012b5e2fc28efbe78beabd5d07e23209605d3530
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-04-07 15:08:06 -0400

    Include Upper facilities

--- lib/src/core/helpers/helpers.dart
+++ lib/src/core/helpers/helpers.dart
@@ -4,4 +4,16 @@ class Helper {
   static String getValidation(){
     return '';
   }
+
+  static String capitalize(String text, [bool lower = true]) {
+    if (text.isEmpty) return text;
+    return text[0].toUpperCase() +
+        (lower ? text.substring(1).toLowerCase() : text.substring(1));
+  }
+
+  static String upper(String text, [bool lower = true]) {
+    if (text.isEmpty) return text;
+    return text.toUpperCase();
+  }
+
 }
