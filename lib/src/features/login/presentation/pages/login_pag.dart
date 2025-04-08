673d60c5624f9ea54a3954a1affd7e61038c7576
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-04-08 15:47:57 -0400

    Mini changes in login page

--- lib/src/features/login/presentation/pages/login_pag.dart
+++ lib/src/features/login/presentation/pages/login_pag.dart
@@ -171,7 +171,7 @@ class MyLoginPageState extends State<LoginPage> {
                             width: AppSpacing.xs,
                           ),
                           Text(
-                            "Secure Contact: ${_version != 'Cargando...' ? _version : emptyString}. Flembee Inc.",
+                            "Secure Contact: ${_version != 'Cargando...' ? _version : emptyString}. Flembee Tech. C.A.",
                             style: const TextStyle(color: Colors.white),
                           ),
                         ],
